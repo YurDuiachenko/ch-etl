35c8d3a27594b0d7323b016317ad57c9c6d8c2bf
YurDuiachenko <d.yurok22@mail.ru>
2024-09-05 15:15:13 +0300

    restore normal migrate.sh

--- db/cmd/migrate.sh
+++ db/cmd/migrate.sh
@@ -3,13 +3,7 @@ set -e
 
 run_sql() {
     sql_file=$(cat $1)
-
-    # local sqls=()
-    # IFS=$'\n\n' read -d '' -ra sqls <<< "$sql_file"
-
-    # for sql in "${sqls[@]}"; do
     clickhouse-client --query="$sql_file"
-    # done
 }
 
 migrations_dir=./migrations/
